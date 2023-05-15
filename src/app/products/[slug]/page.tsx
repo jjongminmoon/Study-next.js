3c8cfed3343236abd96a01455057247d0d398a3e
jjongminmoon <mjm7042@kakao.com>
2023-05-15 16:20:01 +0900

    Dynamic Routing

---
+++ src/app/products/[slug]/page.tsx
@@ -0,0 +1,17 @@
+type Props = {
+  params: {
+    slug: string;
+  };
+};
+
+export default function Pants({ params }: Props) {
+  return <h1>{params.slug} 정보 페이지입니다!</h1>;
+}
+
+export function generateStaticParams() {
+  const products = ["pants", "skirt"];
+
+  return products.map((product) => ({
+    slug: product
+  }));
+}
--- src/app/products/pants/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Pants() {
-  return <h1>바지 정보 페이지입니다!</h1>;
-}
