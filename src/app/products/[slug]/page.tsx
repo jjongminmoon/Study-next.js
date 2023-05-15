e3dc49ad6be505bdc559148389657ffc8693311f
jjongminmoon <mjm7042@kakao.com>
2023-05-15 20:21:21 +0900

    notFound Page

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,3 +1,5 @@
+import { notFound } from "next/navigation";
+
 type Props = {
   params: {
     slug: string;
@@ -5,6 +7,10 @@ type Props = {
 };
 
 export default function Pants({ params }: Props) {
+  if (params.slug === "nothing") {
+    notFound();
+  }
+
   return <h1>{params.slug} 정보 페이지입니다!</h1>;
 }
 
