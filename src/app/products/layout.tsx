6bdbc3d4e07cc6edab5bc942d0e3f201193e0ac3
jjongminmoon <mjm7042@kakao.com>
2023-05-15 22:57:26 +0900

    SEO & Metadata

--- src/app/products/layout.tsx
+++ src/app/products/layout.tsx
@@ -1,8 +1,14 @@
 import Link from "next/link";
 import styles from "./layout.module.css";
+import { Metadata } from "next";
 
 type Props = {
   children: React.ReactNode;
+};
+
+export const metadata: Metadata = {
+  title: "멋진 제품 사이트 | 전체 제품 확인",
+  description: "멋진 제품을 확인해보세요."
 };
 
 export default function ProductsLayout({ children }: Props) {
