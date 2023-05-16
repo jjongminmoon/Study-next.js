5aaf39ce9ac2e572a60ff6979aecf1129af58cef
jjongminmoon <mjm7042@kakao.com>
2023-05-16 16:20:24 +0900

    ISR Rendering

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,6 +1,8 @@
 import { getProduct, getProducts } from "@/service/products";
 import { notFound } from "next/navigation";
 
+export const revalidate = 3;
+
 type Props = {
   params: {
     slug: string;
@@ -20,11 +22,9 @@ export default async function ProductInfoPage({ params: { slug } }: Props) {
     notFound();
   }
 
-  // 서버 파일에 있는 데이터 중 해당 제품의 정보를 찾아서 그걸 보여줌
   return <h1>{product.name} 정보 페이지입니다!</h1>;
 }
 
-// 모든 제품의 페이지들을 미리 만들어 둘 수 있게 해줌 (SSG)
 export async function generateStaticParams() {
   const products = await getProducts();
 
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,21 +1,22 @@
 import Link from "next/link";
-import styles from "./page.module.css";
 import { getProducts } from "@/service/products";
+
+export const revalidate = 3;
 
 export default async function ProductsPage() {
   const products = await getProducts();
-  // 서버 파일(데이터베이스)에 있는 제품의 리스트를 읽어와서, 그걸 보여줌
+
   return (
     <>
       <h1>제품 소개 페이지</h1>
-      <div className={styles.category}>
-        {products.map((product, index) => (
-          <>
-            <Link key={index} href={`/products/${product.id}`}>
-              ● {product.name}
-            </Link>
-          </>
-        ))}
+      <div>
+        <ul>
+          {products.map(({ id, name }, index) => (
+            <li key={index}>
+              <Link href={`/products/${id}`}>{name}</Link>
+            </li>
+          ))}
+        </ul>
       </div>
     </>
   );
