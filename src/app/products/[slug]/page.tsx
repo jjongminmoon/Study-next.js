406c715d786e264115264be71492b641ee335f90
jjongminmoon <mjm7042@kakao.com>
2023-05-16 14:38:10 +0900

    Server Component & Client Component

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -16,10 +16,11 @@ export default function ProductInfoPage({ params }: Props) {
   if (params.slug === "nothing") {
     notFound();
   }
-
+  // 서버 파일에 있는 데이터 중 해당 제품의 정보를 찾아서 그걸 보여줌
   return <h1>{params.slug} 정보 페이지입니다!</h1>;
 }
 
+// 모든 제품의 페이지들을 미리 만들어 둘 수 있게 해줌 (SSG)
 export function generateStaticParams() {
   const products = ["pants", "skirt"];
 
