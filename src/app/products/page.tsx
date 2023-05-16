19c064c46824ff5e3ab43aa9f8320dafd35c80d9
jjongminmoon <mjm7042@kakao.com>
2023-05-16 22:45:13 +0900

    최신 API 라우팅

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,10 +1,8 @@
-import styles from "./page.module.css";
 import Link from "next/link";
 import { getProducts } from "@/service/products";
-import { revalidate } from "./[slug]/page";
 import MeowArticle from "@/components/MeowArticle";
 
-// export const revalidate = 3;
+export const revalidate = 3;
 
 export default async function ProductsPage() {
   const products = await getProducts();
